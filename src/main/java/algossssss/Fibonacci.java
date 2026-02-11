f9900d885200c98168047756184b00b563bd9889
Stanislav Muzulev <muz-stanislav@yandex.ru>
2026-02-11 23:35:28 +0300

    first commit

---
+++ src/main/java/algossssss/Fibonacci.java
@@ -0,0 +1,4 @@
+package algossssss;
+
+public class Fibonacci {
+}
---
+++ src/main/java/algossssss/sorting/SelectionSort.java
@@ -0,0 +1,9 @@
+package algossssss.sorting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class  SelectionSort {
+
+
+}
