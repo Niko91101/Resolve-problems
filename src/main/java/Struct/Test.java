7f1e8e07d89809a7e5d4aedc56766058a160c21d
Stanislav Muzulev <muz-stanislav@yandex.ru>
2026-02-24 23:09:31 +0300

    feat: interface Comparable

--- src/main/java/Struct/Test.java
+++ src/main/java/Struct/Test.java
@@ -5,65 +5,25 @@ import java.util.*;
 public class Test {
     public static void main(String[] args) {
 
-//        List<String> animals = new ArrayList<>();
-//        animals.add("sdqwsdqds");
-//        animals.add("dog");
-//        animals.add("cat");
-//        animals.add("frog");
-//        animals.add("a");
-//        animals.add("sd");
-//        animals.add("bird");
-//
-//        Collections.sort(animals, new StringLengthComparator());
+        List<Person> peopleList = new ArrayList<>();
+        Set<Person> peopleSet = new TreeSet<>();
 
-//        System.out.println(animals);
+        addElements(peopleList);
+        addElements(peopleSet);
 
-        List<Integer> numbers = new ArrayList<>();
+        System.out.println(peopleList);
+        System.out.println(peopleSet);
+    }
 
-        numbers.add(15);
-        numbers.add(1);
-        numbers.add(1500);
-        numbers.add(15000);
-
-        Collections.sort(numbers, new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return o2.compareTo(o1);
-            }
-        });
-        System.out.println(numbers);
-
-        List<Person> people = new ArrayList<>();
-
-        people.add(new Person(3, "Mike"));
-        people.add(new Person(1, "Bob"));
-        people.add(new Person(2, "Katy"));
-
-        people.sort(new Comparator<Person>() {
-            @Override
-            public int compare(Person o1, Person o2) {
-                return Integer.compare(o1.getId(), o2.getId());
-            }
-        });
-
-        System.out.println(people);
+    private static void addElements(Collection collection) {
+        collection.add(new Person(1, "Bob"));
+        collection.add(new Person(2, "Tom"));
+        collection.add(new Person(3, "Katy"));
+        collection.add(new Person(4, "George"));
     }
 }
 
-class StringLengthComparator implements Comparator<String> {
-    @Override
-    public int compare(String o1, String o2) {
-        if (o1.length() > o2.length()) {
-            return 1;
-        } else if (o1.length() < o2.length()) {
-            return -1;
-        } else {
-            return 0;
-        }
-    }
-}
-
-class Person {
+class Person implements Comparable<Person> {
     private int id;
     private String name;
 
@@ -73,15 +33,30 @@ class Person {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+        return id == person.id && Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + Objects.hashCode(name);
+        return result;
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        return Integer.compare(o.name.length(), this.name.length());
+    }
+
+    @Override
     public String toString() {
         return "Person{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
                 '}';
     }
-
-    public int getId() {
-        return id;
-    }
 }
-
