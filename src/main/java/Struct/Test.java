7f4048d1cc3d3cb4df45093d5bccf4d29f6298cc
Stanislav Muzulev <muz-stanislav@yandex.ru>
2026-04-05 15:08:05 +0300

    lesson: Dead lock

--- src/main/java/Struct/Test.java
+++ src/main/java/Struct/Test.java
@@ -1,64 +1,131 @@
 package Struct;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Semaphore;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Test {
 
     public static void main(String[] args) throws InterruptedException {
-        ExecutorService executorService = Executors.newFixedThreadPool(200);
+        Runner runner = new Runner();
 
-        Connection connection = Connection.getConnection();
-        for (int i = 0; i < 10; i++) {
-            executorService.submit(new Runnable() {
-                @Override
-                public void run() {
-                    connection.work();
-                }
-            });
-        }
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                runner.firstThread();
+            }
+        });
 
-        executorService.shutdown();
-        executorService.awaitTermination(1, TimeUnit.DAYS);
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                runner.secondThread();
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        runner.finished();
     }
 }
 
-class Connection {
+class Runner {
+    private Account account1 = new Account();
+    private Account account2 = new Account();
 
-    private static Connection connection = new Connection();
-    private int connectionCount;
-    private final Semaphore semaphore = new Semaphore(10);
+    private final Lock lock1 = new ReentrantLock();
+    private final Lock lock2 = new ReentrantLock();
 
-    private  Connection () {
+    private void takeLocks(Lock lock1, Lock lock2) {
+        boolean firstLockTaken = false;
+        boolean secondLockTaken = false;
+
+        while (true) {
+            try {
+                firstLockTaken = lock1.tryLock();
+                secondLockTaken = lock2.tryLock();
+            } finally {
+                if (firstLockTaken && secondLockTaken) {
+                    return;
+                }
+
+                if (firstLockTaken) {
+                    lock1.unlock();
+                }
+
+                if (secondLockTaken) {
+                    lock2.unlock();
+                }
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
 
     }
 
-    public static Connection getConnection() {
-        return connection;
-    }
+    public void firstThread() {
+        Random random = new Random();
 
-    public void work() throws InterruptedException {
-        semaphore.acquire();
-        try {
-            doWork();
-        } finally {
-            semaphore.release();
+        for (int i = 0; i < 10000; i++) {
+            takeLocks(lock1, lock2);
+
+            try {
+                Account.transfer(account1, account2, random.nextInt(100));
+            } finally {
+                lock1.unlock();
+                lock2.unlock();
+            }
         }
     }
 
-    private void doWork() throws InterruptedException {
-        synchronized (this) {
-            connectionCount++;
-            System.out.println(connectionCount);
-        }
+    public void secondThread() {
+        Random random = new Random();
 
-        Thread.sleep(5000);
+        for (int i = 0; i < 10000; i++) {
+            takeLocks(lock2, lock1);
 
-        synchronized (this) {
-            connectionCount--;
+            try {
+                Account.transfer(account2, account1, random.nextInt(100));
+            } finally {
+                lock2.unlock();
+                lock1.unlock();
+            }
         }
     }
 
+    public void finished() {
+        System.out.println(account1.getBalance());
+        System.out.println(account2.getBalance());
+        System.out.println(("Total balance: " + (account1.getBalance() + account2.getBalance())));
+    }
+}
+
+class Account {
+    private int balance = 10000;
+
+    public void deposit(int amount) {
+        balance += amount;
+    }
+
+    public void withdraw(int amount) {
+        balance -= amount;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public static void transfer(Account acc1, Account acc2, int amount) {
+        acc1.withdraw(amount);
+        acc2.deposit(amount);
+    }
 }
