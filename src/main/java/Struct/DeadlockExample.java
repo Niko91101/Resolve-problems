8fb60f0b50a56289e9c685f2dfa19a3ada825e7c
Stanislav Muzulev <muz-stanislav@yandex.ru>
2026-04-06 22:47:57 +0300

    lesson: lambda on data structures

--- src/main/java/Struct/DeadlockExample.java
+++ src/main/java/Struct/DeadlockExample.java
@@ -1,33 +1,70 @@
 package Struct;
 
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class DeadlockExample {
 
     public static void main(String[] args) throws InterruptedException {
-        List<String> list = new ArrayList<>();
+        int[] array1 = new int[10];
+        List<Integer> list1 = new ArrayList<>();
 
-        list.add("Aasasdad");
-        list.add("Bsasasdx");
-        list.add("Csaqw");
-        list.add("Ds");
-        list.add("Eas");
-        list.add("Fwer");
-        list.add("G");
+        fillArr(array1);
+        fillList(list1);
 
-//        list.sort(new Comparator<String>() {
-//            @Override
-//            public int compare(String o1, String o2) {
-//                return Integer.compare(o1.length(), o2.length());
-//            }
-//        });
+        //map
+        array1 = Arrays.stream(array1)
+                .map(x -> x * 2)
+                .toArray();
 
-        System.out.println(list);;
+        list1 = list1.stream()
+                .map(x -> x * 2)
+                .collect(Collectors.toList());
 
-        list.sort((s1, s2) ->  {
-           return Integer.compare(s1.length(), s2.length());
-        });
+        //filter
+
+
+//        System.out.println(Arrays.toString(array1));
+//        System.out.println(list1);
+
+        int[] array2 = new int[10];
+        List<Integer> list2 = new ArrayList<>();
+
+        fillArr(array2);
+        fillList(list2);
+
+        array2 = Arrays.stream(array2)
+                .filter(x -> x % 2 == 0)
+                .toArray();
+
+        //System.out.println(Arrays.toString(array2));
+
+        //forEach
+
+        //reduce
+
+        //
+
+        int result = Arrays.stream(array2)
+                .reduce((acc, b) -> acc + b)
+                .orElse(-1);
+
+        System.out.println(result);
+
+
+    }
+
+    private static void fillArr(int[] array) {
+        for (int i = 0; i < 10; i++) {
+            array[i] = i + 1;
+        }
+    }
+
+    private static void fillList(List<Integer> list) {
+        for (int i = 0; i < 10; i++) {
+            list.add(i + 1);
+        }
     }
 }
